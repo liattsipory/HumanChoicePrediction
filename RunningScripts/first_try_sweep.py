98706bacd2ff719a1c1bafd2272bf986afe63121
Levi-Noa <noa-levi@campus.technion.ac.il>
2024-04-19 11:15:24 +0300

    new sweep

---
+++ RunningScripts/first_try_sweep.py
@@ -0,0 +1,35 @@
+import wandb
+YOUR_WANDB_USERNAME = "noa7566"
+project = "NLP2024_PROJECT_207897091_322720103"
+
+command = [
+        "${ENVIRONMENT_VARIABLE}",
+        "${interpreter}",
+        "StrategyTransfer.py",
+        "${project}",
+        "${args}"
+    ]
+
+
+sweep_config_1 = {
+    "name": "LSTM: SimFactor=0/4 for any features representation",
+    "method": "grid",
+    "metric": {
+        "goal": "maximize",
+        "name": "AUC.test.max"
+    },
+    "parameters": {
+        "ENV_HPT_mode": {"values": [False]},
+        "architecture": {"values": ["LSTM"]},
+        "seed": {"values": list(range(1, 6))},
+        "online_simulation_factor": {"values": [0, 4]},
+        "features": {"values": ["EFs", "GPT4", "BERT"]},
+        "basic_nature": {"values": [18,19,20]},
+    },
+    "command": command
+}
+
+# Initialize a new sweep
+sweep_id = wandb.sweep(sweep=sweep_config_1, project=project)
+print("run this line to run your agent in a screen:")
+print(f"screen -dmS \"sweep_agent\" wandb agent {YOUR_WANDB_USERNAME}/{project}/{sweep_id}")
