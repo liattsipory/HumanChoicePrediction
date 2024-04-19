8275644da67aba06eff65275fd5dea3c8fbdec4c
liattsipory <liat.tsipory@campus.technion.ac.il>
2024-04-19 13:24:13 +0300

    HI

--- RunningScripts/final_sweep_207897091_322720103.py
+++ RunningScripts/final_sweep_207897091_322720103.py
@@ -1,5 +1,5 @@
 import wandb
-YOUR_WANDB_USERNAME = "noa7566"
+YOUR_WANDB_USERNAME = "noa-levi"
 project = "NLP2024_PROJECT_207897091_322720103"
 
 command = [
@@ -11,24 +11,42 @@ command = [
     ]
 
 sweep_config_1 = {
-    "name": "LSTM: SimFactor=0/4 for any features representation",
+    "name": "decide between relaxed strategies",
     "method": "grid",
-    "metric": {
-        "goal": "maximize",
-        "name": "AUC.test.max"
-    },
+    # "metric": {
+    #     "goal": "maximize",
+    #     "name": "AUC.test.max"
+    # },
     "parameters": {
-        "ENV_HPT_mode": {"values": [False]},
-        "architecture": {"values": ["LSTM"]},
+        "ENV_HPT_mode": {"values": [True]},
         "seed": {"values": list(range(1, 6))},
         "online_simulation_factor": {"values": [0, 4]},
         "features": {"values": ["EFs", "GPT4", "BERT"]},
-        #"basic_nature": {"values": [18,19,20]},
+        "basic_nature": {"values": [17,18,19,20,21,22,23,24]},
     },
     "command": command
 }
 
+sweep_config_0 = {
+    "name": "initialize",
+    "method": "grid",
+    # "metric": {
+    #     "goal": "maximize",
+    #     "name": "AUC.test.max"
+    # },
+    "parameters": {
+        "ENV_HPT_mode": {"values": [True]},
+        "seed": {"values": list(range(1, 6))},
+        "online_simulation_factor": {"values": [0, 4]},
+        "features": {"values": ["EFs", "GPT4", "BERT"]},
+    },
+    "command": command
+}
 # Initialize a new sweep
-sweep_id = wandb.sweep(sweep=sweep_config_1, project=project)
+# sweep_id = wandb.sweep(sweep=sweep_config_1, project=project)
+# print("run this line to run your agent in a screen:")
+# print(f"screen -dmS \"sweep_agent\" wandb agent {YOUR_WANDB_USERNAME}/{project}/{sweep_id}")
+
+sweep_id = wandb.sweep(sweep=sweep_config_0, project=project)
 print("run this line to run your agent in a screen:")
 print(f"screen -dmS \"sweep_agent\" wandb agent {YOUR_WANDB_USERNAME}/{project}/{sweep_id}")
