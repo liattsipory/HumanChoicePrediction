8328ad369173bdf2f4ed820ee05105c8bd2ac162
Levi-Noa <noa-levi@campus.technion.ac.il>
2024-04-19 11:15:05 +0300

    new sweep

---
+++ RunningScripts/clone_and_init_207897091_322720103.sh
@@ -0,0 +1,13 @@
+# cloning your code from github:
+# git clone https://github.com/eilamshapira/NLP_project.git
+
+git clone https://github.com/liattsipory/HumanChoicePrediction.git
+python first_try_sweep.py
+
+# Your main sweep:
+#python final_sweep_final_sweep_207897091_322720103.py
+
+# More runs appear in your report:
+# python sweep_1.py
+# python sweep_2.py
+# python sweep_3.py
