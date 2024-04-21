30ed2300b91817204a8c6c993a7f2f13b8040a44
Levi-Noa <noa-levi@campus.technion.ac.il>
2024-04-21 09:54:14 +0300

    fix_sweeps

--- RunningScripts/final_sweep_207897091_322720103.py
+++ RunningScripts/final_sweep_207897091_322720103.py
@@ -13,10 +13,22 @@ command = [
 sweep_config_1 = {
     "name": "decide between relaxed strategies",
     "method": "grid",
-    # "metric": {
-    #     "goal": "maximize",
-    #     "name": "AUC.test.max"
-    # },
+    "metric": {
+        "goal": "maximize",
+        "name": "AUC.test.max"
+    },
+    "parameters": {
+        "ENV_HPT_mode": {"values": [True]},
+        "seed": {"values": list(range(1, 6))},
+        "online_simulation_factor": {"values": [0, 4]},
+        #"features": {"values": ["EFs", "GPT4", "BERT"]},
+        "basic_nature": {"values": [17,18,19,20,21,22,23,24]},
+    },
+    "command": command
+}
+sweep_config_1_no_metric = {
+    "name": "decide between relaxed strategies",
+    "method": "grid",
     "parameters": {
         "ENV_HPT_mode": {"values": [True]},
         "seed": {"values": list(range(1, 6))},
@@ -27,39 +39,39 @@ sweep_config_1 = {
     "command": command
 }
 
-sweep_config_0 = {
-    "name": "initialize",
-    "method": "grid",
-    "parameters": {
-        "ENV_HPT_mode": {"values": [True]},
-        "seed": {"values": list(range(1, 6))},
-        "basic_nature": {"values": [17,18,19,20,21,22,23,24]},
-    },
-    "command": command
-}
-sweep_config = {
-    "name": "TRYING TO FIX THE BUGS",
-    "method": "grid",
-    "metric": {
-        "goal": "maximize",
-        "name": "AUC.test.max"
-    },
-    "parameters": {
-        "ENV_HPT_mode": {"values": [False]},
-        "seed": {"values": list(range(1, 6))},
-    },
-    "command": command
-}
+# sweep_config_0 = {
+#     "name": "initialize",
+#     "method": "grid",
+#     "parameters": {
+#         "ENV_HPT_mode": {"values": [True]},
+#         "seed": {"values": list(range(1, 6))},
+#         "basic_nature": {"values": [17,18,19,20,21,22,23,24]},
+#     },
+#     "command": command
+# }
+# sweep_config = {
+#     "name": "TRYING TO FIX THE BUGS",
+#     "method": "grid",
+#     "metric": {
+#         "goal": "maximize",
+#         "name": "AUC.test.max"
+#     },
+#     "parameters": {
+#         "ENV_HPT_mode": {"values": [False]},
+#         "seed": {"values": list(range(1, 6))},
+#     },
+#     "command": command
+# }
 
-sweep_config_00 = {
-    "name": "TRYING TO FIX THE BUGS",
-    "method": "grid",
-    "parameters": {
-        "ENV_HPT_mode": {"values": [True]},
-        "seed": {"values": list(range(1, 6))},
-    },
-    "command": command
-}
+# sweep_config_00 = {
+#     "name": "TRYING TO FIX THE BUGS",
+#     "method": "grid",
+#     "parameters": {
+#         "ENV_HPT_mode": {"values": [True]},
+#         "seed": {"values": list(range(1, 6))},
+#     },
+#     "command": command
+# }
 
 # sweep_id = wandb.sweep(sweep=sweep_config, project=project)
 # print("run this line to run your agent in a screen:")
